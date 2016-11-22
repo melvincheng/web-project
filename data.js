633c17876dc6b9a40076c5ca41c2fb500c89e218
melvincheng <melvin.cheng@live.com>
2016-11-22 13:39:45 -0500

    Fully implemented collapsible headers, and articles. Added images to articles. Some styling added. Sorting by source implemented

--- data.js
+++ data.js
@@ -10,34 +10,108 @@ $(document).ready(function () {
 		return $.ajax({
 			url:'https://newsapi.org/v1/sources',
 			success: function(data) {
-				console.log(data);
+//				console.log(data);
 				sources = $(data).attr('sources');
 			}
 		});
 	}
 	
-	function getArticles() {
+	function addArticleSource(data, sourceDiv) {
+		var pulledArticle = $(data).attr('articles');
+//		console.log(data);
+		for(var i = 0; i < pulledArticle.length; i++) {
+			var currentArticle = pulledArticle[i];
+			var article = $('<div>');
+			var title = $('<div>');
+			var author = $('<div>');
+			var description = $('<div>');
+			var url = $('<a>');
+			var img = $('<img>').attr('src',$(currentArticle).attr('urlToImage'));
+			var content = $('<div>');
+			$(img).attr('width','100%');
+
+			title.append(url);
+			title.addClass('title');
+				
+			$(title).click(function() {
+				$(this).siblings().each(function() {
+					if($(this).hasClass('collapse')){
+						$('.collapse-in').addClass('collapse');
+						$('.collapse-in').removeClass('collapse-in');
+						$(this).removeClass('collapse');
+						$(this).addClass('collapse-in');
+						
+					}else{
+						$(this).addClass('collapse');
+						$(this).removeClass('collapse-in');
+					}
+				});
+			});
+			
+			
+			author.append('By: ' + $(currentArticle).attr('author'));
+			author.addClass('author');
+
+			description.append($(currentArticle).attr('description'));
+			description.addClass('description');
+
+			url.attr('href', $(currentArticle).attr('url'));
+			url.append($(currentArticle).attr('title'));
+			url.addClass('link');
+			
+			content.append(img);
+			content.append(author);
+			content.append(description);
+			content.attr('id','content');
+			content.addClass('collapse');
+
+			article.append(title);
+			article.append(content);
+			article.addClass('article');
+
+			sourceDiv.append(article);
+			
+		}
+	}
+	
+	function displaySource() {
 		var sourceLink;
+		var sourceArticleDivList = [];
+		var sourceId = [];
 		return $.each(sources, function(index, data){
-			console.log(data);
+//			console.log(data);
+			
 			var sourceName = $('<div>');
 			var sourceLogo = $('<img>').attr('src', $($(data).attr('urlsToLogos')).attr('large')).attr('height','48');
+			var sourceDiv = $('<div>').attr('id',$(data).attr('name'));
+			var sourceArticle = $('<div>').addClass('collapse').attr('id',$(data).attr('id'));
+			
+			sourceDiv.append(sourceName);
+			sourceDiv.append(sourceArticle);
+			
+			sourceArticleDivList.push(sourceArticle);
+			
 			sourceName.append(sourceLogo);
 			sourceName.append($(data).attr('name'));
 			sourceName.addClass('sortHeader');
-			$(body).append(sourceName);
+			sourceName.attr('data-toggle','collapse').attr('data-target','#' + $(data).attr('id'));
+			
+			$(body).append(sourceDiv);
+			
 			var sourceOrigin = $(data).attr('id');
+			sourceId.push(sourceOrigin);
 			sourceLink = 'https://newsapi.org/v1/articles?source=' + sourceOrigin + '&apiKey=98432bdc8b0d474797f981385df66c5f';
 			$.ajax({
 				url: sourceLink,
 				success: function(data) {
 					articleList.push(data);
+					addArticleSource(data, sourceArticleDivList[sourceId.indexOf($(data).attr('source'))]);
 				}
 			});
-		});
+		});	
 	}
 	
-	function mainCategory() {
+	function displayCategory() {
 		var category = ['business', 'entertainment', 'gaming', 'general', 'music', 'science-and-nature', 'sport', 'technology'];
 		var categoryName = ['Business', 'Entertainment', 'Gaming', 'General', 'Music', 'Science and nature', 'Sports', 'Technology'];
 		var categoryDiv = [$('<div>'), $('<div>'), $('<div>'), $('<div>'), $('<div>'), $('<div>'), $('<div>'), $('<div>')];
@@ -50,7 +124,7 @@ $(document).ready(function () {
 			categoryArticles.push(tempArticle);
 			
 			tempArticle.attr('id', category[i]+'Articles').addClass('collapse');
-			tempDiv.attr('id', category[i]).append($('<header>').append(categoryName[i]).addClass('sortHeader').attr('data-toggle','collapse').attr('data-target','#' + category[i] + 'Articles')).append(tempArticle);
+			tempDiv.attr('id', category[i]).addClass('category').append($('<header>').append(categoryName[i]).addClass('sortHeader').attr('data-toggle','collapse').attr('data-target','#' + category[i] + 'Articles')).append(tempArticle);
 			$(body).append(categoryDiv[i]);
 		}
 //		console.log(categoryArticles);
@@ -116,30 +190,51 @@ $(document).ready(function () {
 
 	function addArticleCategory(data, category) {
 //		console.log(data);
-		for(var j=0; j < $(data).attr('articles').length; j++){
-			var pulledArticle = $(data).attr('articles');
+		var pulledArticle = $(data).attr('articles');
+		for(var j=0; j < pulledArticle.length; j++){
+			var currentArticle = pulledArticle[j];
 			var article = $('<div>');
 			var title = $('<div>');
 			var author = $('<div>');
+			var source = $('<div>');
 			var description = $('<div>');
 			var url = $('<a>');
-
+			var img = $('<img>').attr('src',$(currentArticle).attr('urlToImage'));
 			var content = $('<div>');
-
+			$(img).attr('width','100%');
 
 			title.append(url);
 			title.addClass('title');
-
-			author.append('By: ' + $(pulledArticle[j]).attr('author'));
+				
+			$(title).click(function() {
+				$(this).siblings().each(function() {
+					if($(this).hasClass('collapse')){
+						$('.collapse-in').addClass('collapse');
+						$('.collapse-in').removeClass('collapse-in');
+						$(this).removeClass('collapse');
+						$(this).addClass('collapse-in');
+						
+					}else{
+						$(this).addClass('collapse');
+						$(this).removeClass('collapse-in');
+					}
+				});
+			});
+			
+			source.append('Source: ' + $(data).attr('source'));
+			
+			author.append('By: ' + $(currentArticle).attr('author'));
 			author.addClass('author');
 
-			description.append($(pulledArticle[j]).attr('description'));
+			description.append($(currentArticle).attr('description'));
 			description.addClass('description');
 
-			url.attr('href', $(pulledArticle[j]).attr('url'));
-			url.append($(pulledArticle[j]).attr('title'));
+			url.attr('href', $(currentArticle).attr('url'));
+			url.append($(currentArticle).attr('title'));
 			url.addClass('link');
-
+			
+			content.append(img);
+			content.append(source);
 			content.append(author);
 			content.append(description);
 			content.attr('id','content');
@@ -152,15 +247,7 @@ $(document).ready(function () {
 			category.append(article);
 		}
 	}
-	
-	function mainSource() {
-		for(var i = 0; i < articleList.length; i++) {
-			
-		}
-	}
-	
-	function addArticleSource(data, source) {
-		
-	}
-	getSource().then(getArticles)//.then(mainCategory);
+
+	getSource().then(displaySource);
+//	getSource().then(displayCategory);
 });
